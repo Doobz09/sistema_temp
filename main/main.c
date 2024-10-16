4515b4dab4d2da93d7e4679edf108e5f6ee53277
GpMendez <prueba@gmail.com>
2024-10-15 20:21:06 -0600

    se implemento el algoritmo knn

--- main/main.c
+++ main/main.c
@@ -3,6 +3,18 @@
 #include <freertos/task.h> 
 #include "esp_mac.h"
 #include "../bsp/bsp.h"
+#include <math.h>
+typedef struct {
+    double x, y, z;  // Coordenadas del punto
+    int label;    // Clase del punto (0 o 1)
+} Point;
+
+
+#include"datos.h"
+
+
+
+
 char state[100];
 
 #define ACTIVO 0
@@ -13,12 +25,14 @@ TaskHandle_t adc_2_task_handle = NULL;
 TaskHandle_t adc_3_task_handle = NULL;
 TaskHandle_t uart_task_handle = NULL;
 TaskHandle_t activar_desactivar_task_handle = NULL;
+TaskHandle_t knn_task_handle = NULL;
 
 int value_adc=0;
 int value_adc_2=0;
 int value_adc_3=0;
 int bandera_control =0;
 int count_btn=0;
+int indice =0;
 
 bool sistema_on =false;
 
@@ -28,7 +42,7 @@ void adc_2_task(void* pvParameters);
 void adc_3_task(void* pvParameters);
 void uart_task(void* pvParameters);
 void activar_desactivar_task(void* pvParameters);
-
+void knn_task(void* pvParameters);
 
 
 void app_main(void)
@@ -48,6 +62,7 @@ void app_main(void)
                 sistema_on = true;
                 xTaskCreate(uart_task, "uart Task", 2048, NULL, 5, &uart_task_handle);
                 xTaskCreate(activar_desactivar_task, "activar_desactivar_task", 2048, NULL, 5, &activar_desactivar_task_handle );
+                xTaskCreate(knn_task, "knn_task", 32768, NULL, 5, &knn_task_handle );
             }     
             else{
                  hal_led_set_level(LED4,0);
@@ -78,6 +93,12 @@ void app_main(void)
                     vTaskDelete(activar_desactivar_task_handle);
                    activar_desactivar_task_handle = NULL; 
                 }
+                if(knn_task_handle != NULL){
+                    vTaskDelete(knn_task_handle);
+                   knn_task_handle = NULL; 
+                }
+
+
                  
 
             }
@@ -95,36 +116,37 @@ void uart_task(void *params) {
              hal_terminal_send(state);
         }
         else if (bandera_control==11){
-            sprintf(state, "11,%d\n",value_adc);
+            sprintf(state, "11,%d,%d\n",value_adc,indice);
             hal_terminal_send(state);
         }
         else if(bandera_control==12){
-            sprintf(state, "12,%d\n",value_adc_2);
+            sprintf(state, "12,%d,%d\n",value_adc_2,indice);
             hal_terminal_send(state);
 
         }
         else if(bandera_control==13){
-            sprintf(state, "13,%d\n",value_adc_3);
+            sprintf(state, "13,%d,%d\n",value_adc_3,indice);
             hal_terminal_send(state);
 
         }
         else if(bandera_control==212){
-            sprintf(state, "212,%d,%d\n",value_adc,value_adc_2);
+            sprintf(state, "212,%d,%d,%d\n",value_adc,value_adc_2,indice);
             hal_terminal_send(state);
 
         }
         else if(bandera_control==213){
-            sprintf(state, "213,%d,%d\n",value_adc,value_adc_3);
+            sprintf(state, "213,%d,%d,%d\n",value_adc,value_adc_3,indice);
             hal_terminal_send(state);
 
         }
         else if(bandera_control==223){
-            sprintf(state, "223,%d,%d\n",value_adc_2,value_adc_3);
+            sprintf(state, "223,%d,%d,%d\n",value_adc_2,value_adc_3,indice);
             hal_terminal_send(state);
 
         }
         else if(bandera_control==3){
-            sprintf(state, "3,%d,%d,%d\n",value_adc,value_adc_2,value_adc_3);
+            //sprintf(state, "3,%d,%d,%d\n",value_adc,value_adc_2,value_adc_3);
+            sprintf(state, "3,%d,%d,%d,%d\n",value_adc,value_adc_2,value_adc_3,indice);
             hal_terminal_send(state);
 
         }
@@ -223,7 +245,7 @@ void activar_desactivar_task(void *params){
 void adc_task(void *params) {
     while (1) {
         value_adc = hal_read_adc(ADC1_CHANNEL_4);
-        value_adc = value_adc/12;
+        value_adc = (value_adc/12)-32;
         vTaskDelay(100 / portTICK_PERIOD_MS); // Espera 100ms
     }
 }
@@ -231,7 +253,7 @@ void adc_task(void *params) {
 void adc_2_task(void *params){
     while(1){
         value_adc_2 = hal_read_adc(ADC1_CHANNEL_5);
-        value_adc_2 = value_adc_2/12;
+        value_adc_2 = (value_adc_2/12)-32;
         vTaskDelay(100 / portTICK_PERIOD_MS); // Espera 100ms
 
     }
@@ -239,9 +261,131 @@ void adc_2_task(void *params){
 void adc_3_task(void *params){
     while(1){
         value_adc_3 = hal_read_adc(ADC1_CHANNEL_6);
-        value_adc_3 = value_adc_3/12;
+        value_adc_3 = (value_adc_3/12)-32;
         vTaskDelay(100 / portTICK_PERIOD_MS); // Espera 100ms
 
     }
 }
+
+#define K 3
+#define NUM_POINTS 3000 
+
+// Función para calcular la distancia euclidiana entre dos puntos
+double euclidean_distance(Point a, Point b, int n) {
+    if(n==3)
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)+ pow(a.z - b.z, 2));
+    
+    else if(n==2)
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    
+    else if(n==1)
+        return sqrt(pow(a.x - b.x, 2));
+
+    return 0;
+        
+}
+
+// Función para encontrar los K vecinos más cercanos
+void find_k_nearest_neighbors(Point dataset[], Point test_point, int k, int neighbors[],int n) {
+    double distances[NUM_POINTS];
+    int i, j;
+
+    // Calcular las distancias entre el punto de prueba y todos los puntos en el dataset
+    for (i = 0; i < NUM_POINTS; i++) {
+        distances[i] = euclidean_distance(dataset[i], test_point,n);
+    }
+
+    // Ordenar los puntos por distancia usando una selección simple de k vecinos
+    for (i = 0; i < k; i++) {
+        double min_distance = 1e9;
+        int min_index = -1;
+        for (j = 0; j < NUM_POINTS; j++) {
+            if (distances[j] < min_distance) {
+                min_distance = distances[j];
+                min_index = j;
+            }
+        }
+        neighbors[i] = dataset[min_index].label;  // Almacenar la etiqueta del vecino
+        distances[min_index] = 1e9;               // Excluir el punto ya seleccionado
+    }
+}
+
+int classify(Point dataset[], Point test_point, int k,int n) {
+    int neighbors[K];
+    int i, label0 = 0, label1 = 0,label2 = 0;
+
+    // Encontrar los k vecinos más cercanos
+    find_k_nearest_neighbors(dataset, test_point, k, neighbors,n);
+
+    // Contar las etiquetas de los vecinos
+    for (i = 0; i < k; i++) {
+        if (neighbors[i] == 1) label0++;
+        else if (neighbors[i] == 2) label1++;
+        else if (neighbors[i] == 3) label2++;
+    }
+
+	    // Devolver la etiqueta mayoritaria
+	if (label0 > label1 && label0 > label2) {
+	    return 0;
+	} else if (label1 > label0 && label1 > label2) {
+	    return 1;
+	} else {
+	    return 2;
+	}
+}
+
+
+void knn_task(void *params){
+    Point a;
+    int n=0;
+
+    while(sistema_on==true){
+        if(bandera_control==3){
+            a.x = value_adc;
+            a.y = value_adc_2;
+            a.z = value_adc_3;
+            n=3;
+
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==11){
+            a.x = value_adc;
+            n=1;
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==12){
+            a.x = value_adc_2;
+            n=1;
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==13){
+            a.x = value_adc_3;
+            n=1;
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==212){
+            a.x = value_adc;
+            a.y = value_adc_2;
+            n=2;
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==213){
+            a.x = value_adc;
+            a.y = value_adc_3;
+            n=2;
+            indice = classify(dataset, a, K,n);
+        }
+        if(bandera_control==223){
+            a.x = value_adc_2;
+            a.y = value_adc_3;
+            n=2;
+            indice = classify(dataset, a, K,n);
+        }
+
+        
+        vTaskDelay(100 / portTICK_PERIOD_MS); // Espera 100ms
+
+    }
+}
+
 
